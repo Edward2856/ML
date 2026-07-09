2a7611291e87e0c3dedf29dd793161c89316a552
Edward2856 <edwardsaveri7@gmail.com>
2026-07-09 15:34:25 +0530

    added mnist accuracy plot

--- BNN Implementation/accuracy_plot.py
+++ BNN Implementation/accuracy_plot.py
@@ -1,14 +1,14 @@
 import torch
 import matplotlib.pyplot as plt
 
-acc = torch.load('cifar_accuracy.pt')
-acc_real = torch.load('cifar_real_accuracy.pt')
+acc = torch.load('mnist_conv_accuracy.pt')
+acc_real = torch.load('mnist_real_conv_accuracy.pt')
 x = range(1, len(acc) + 1)
-plt.plot(x, [a*100 for a in acc], linewidth=2, color='blue', label='CIFAR-10 BNN')
-plt.plot(x, [a*100 for a in acc_real], linewidth=2, color='red', label='CIFAR-10 Real')
-plt.title('CIFAR-10 Accuracy over Epochs')
+plt.plot(x, [a*100 for a in acc], linewidth=2, color='blue', label='MNIST BNN')
+plt.plot(x, [a*100 for a in acc_real], linewidth=2, color='red', label='MNIST Real')
+plt.title('MNIST (Convolutional) Accuracy over Epochs')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy (%)')
 plt.grid()
 plt.legend()
-plt.savefig('cifar_accuracy.png')
+plt.savefig('mnist_conv_accuracy.png')
